a1a9847e3e0502ea69ca3e837aff05eac0ca4008
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2020-01-12 17:25:48 +0100

    nicer two_columns

--- notebooks/show_html.js
+++ notebooks/show_html.js
@@ -1,3 +1,31 @@
+// css
+console.log("loading show_html")
+var css = `
+.show-html-top {
+    display:grid;
+    grid-template-columns: auto auto;
+}
+div.show-html-cell {
+    border-radius: 10px;
+    padding: 10px;
+}
+div.show-html-left {
+    border: 1px solid #ddd;
+    /*background-color: #ccc;*/
+    margin-right: 20px;
+}
+div.show-html-right {
+    border: 1px solid #00f;
+    background-color: #dd;
+    margin-left: 20px;
+}    
+`;
+
+function injected_css() {
+    return `<style>${css}</style>`;
+}
+
+// helper
 function escape(string) {
     return string
         .replace(/</g, '&lt;')
@@ -12,10 +40,12 @@ function one_column(fragment) {
 }
 
 function two_columns(fragment) {
-    let left = `<pre><code>${escape(fragment)}</code></pre>`;
+    let left = `<pre><code class="show-html-cell show-html-left">${escape(fragment)}</code></pre>`;
     let right = fragment;
-    let whole = `<div style="display:grid; grid-template-columns: auto auto;"><div>${left}</div><div>${right}</div></div>`;
-    return $$.html(whole);
+    let whole = `<div class="show-html-top">
+<div class="show-html-left">${left}</div><div class="show-html-cell show-html-right">${right}</div>
+</div>`;
+    return $$.html(injected_css() + whole);
 }
 
 exports.one_column = one_column;
