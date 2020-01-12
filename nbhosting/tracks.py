8043dd871718656da54c59525172d468e4f35fc4
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2020-01-12 15:29:02 +0100

    tracks

---
+++ nbhosting/tracks.py
@@ -0,0 +1,19 @@
+"""
+This module is for nbhosting
+"""
+
+from nbhosting.courses import (
+    Track, Section, Notebook,
+    notebooks_by_pattern, track_by_directory)
+
+def tracks(coursedir):
+    return [
+        Track(coursedir,
+              name="html basics",
+              sections=[
+                Section(name="html basics",
+                        coursedir=coursedir,
+                        notebooks=notebooks_by_pattern(coursedir,"1*.md")),
+                ],
+              description="Intro aux outils Web frontend"),
+        ]
