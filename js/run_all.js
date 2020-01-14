b3b4d5e8d6578e442dff3d9dc4a86642f5c27b7c
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2020-01-14 11:34:27 +0100

    nicer run all button

--- js/run_all.js
+++ js/run_all.js
@@ -1,6 +1,14 @@
 function run_all_button() {
-    let html = `<span>Tip&nbsp;:
-<button onclick="Jupyter.notebook.execute_all_cells()">Run all cells</button>
+    let html = `<style>
+span.run-all {
+    border: 1px solid black;
+    background-color: #50d97b;
+    padding: 6px 20px;
+    border-radius: 8px;
+}
+</style>
+<span>Tip&nbsp;:
+<span class="run-all" onclick="Jupyter.notebook.execute_all_cells()">Click this button to run all cells</span>
 before running the slideshow</span>
 `;
     $$.html(html);
