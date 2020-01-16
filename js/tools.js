a565eb600b01e4810e9996a2fb87bb00bea4453f
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2020-01-16 18:49:42 +0100

    more consistent naming scheme for js files

--- js/tools.js
+++ js/tools.js
@@ -1,15 +1,15 @@
 //////////
-var show_html = require('./show_html');
+var show_html = require('./show-html');
 
 exports.one_column = show_html.one_column;
 exports.two_columns = show_html.two_columns;
 exports.iframe_html_css = show_html.iframe_html_css;
 
 //////////
-var inject_css = require('./inject_css');
+var inject_css = require('./inject-css');
 
 //////////
-var run_all = require('./run_all');
+var run_all = require('./run-all');
 
 function init() {
     let css = inject_css.read_style('../css/in-out.css');
